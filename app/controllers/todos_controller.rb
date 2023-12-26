85c5b980887f4be712407cc5b840edbdc4bcd6d5
yume-ebina <cityeventa@gmail.com>
2023-12-26 10:01:07 +0900

    finish 2-7

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,57 +1,56 @@
 class TodosController < ApplicationController
-  before_action :set_todo, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!
+  before_action :set_goal
+  before_action :set_todo, only: %i[ show edit update destroy sort]
 
-  # GET /todos
-  def index
-    @todos = Todo.all
-  end
-
-  # GET /todos/1
-  def show
-  end
 
   # GET /todos/new
   def new
-    @todo = Todo.new
+    @todo = @goal.todos.new
   end
 
   # GET /todos/1/edit
   def edit
   end
 
+  def sort
+  end
+
   # POST /todos
   def create
-    @todo = Todo.new(todo_params)
+    @todo = @goal.todos.new(todo_params)
 
     if @todo.save
-      redirect_to @todo, notice: "Todo was successfully created."
+      @status = true
     else
-      render :new, status: :unprocessable_entity
+      @status = false
     end
   end
 
   # PATCH/PUT /todos/1
   def update
     if @todo.update(todo_params)
-      redirect_to @todo, notice: "Todo was successfully updated.", status: :see_other
+      @status = true
     else
-      render :edit, status: :unprocessable_entity
+      @status = false
     end
   end
 
   # DELETE /todos/1
   def destroy
     @todo.destroy!
-    redirect_to todos_url, notice: "Todo was successfully destroyed.", status: :see_other
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_todo
-      @todo = Todo.find(params[:id])
+    def set_goal
+      @goal = current_user.goals.find_by(id: params[:goal_id])
+      redirect_to(goals_url, alert: "ERROR!!") if @goal.blank?
     end
 
-    # Only allow a list of trusted parameters through.
+    def set_todo
+      @todo = @goal.todos.find_by(params[:id])
+    end
+
     def todo_params
       params.require(:todo).permit(:content, :goal_id, :position, :done)
     end
